5bb7458e0b8e078123105978e050e4aff2522a3d
svz_code <arducat.fteam@xmail.ru>
2026-01-21 20:09:54 +0300

    исправная версия pseudo_exe.asm

--- Vodka/pseudo_exe/pseudo_exe.asm
+++ Vodka/pseudo_exe/pseudo_exe.asm
@@ -1,7 +1,7 @@
 format PE CONSOLE
 entry start
 
-section '.text' code readable executable align 512
+section '.text' code readable executable
 start:
 	mov eax, 1
 	mov ebx, 332 ; так задумано, но в Vodka я не смог посмотреть код ошибки
